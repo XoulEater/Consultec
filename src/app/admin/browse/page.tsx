ce7ca70ba5183c956b71b989403134c9fa40ebb3
XoulEater <ncqueescribir@gmail.com>
2025-05-03 17:48:24 -0600

    Fetch and display teachers with loading state; implement data fetching logic

--- src/app/admin/browse/page.tsx
+++ src/app/admin/browse/page.tsx
@@ -1,8 +1,39 @@
+"use client";
 import { Searchbar } from "@/components/Searchbar";
 import { Table } from "@/components/Table";
-import { Suspense } from "react";
+import { TeachersTable } from "@/lib/types";
+import { getTeachers } from "@/services/teacher.service";
+import { useSearchParams } from "next/navigation";
+import { Suspense, useEffect, useState } from "react";
 
 export default function Home() {
+    const searchParams = useSearchParams();
+    const [teachers, setTeachers] = useState<TeachersTable[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        setLoading(true); // Set loading to true before fetching
+        const fetchTeachers = async () => {
+            try {
+                const data = await getTeachers(searchParams);
+
+                // TODO: Remove this filter when the backend is ready
+                setTeachers(
+                    data.filter((teacher) => teacher.lastUpdate !== null)
+                );
+                console.log(
+                    "Fetched teachers:",
+                    data.filter((teacher) => teacher.lastUpdate !== null)
+                ); // Debugging line
+
+                setLoading(false); // Set loading to false after fetching
+            } catch (error) {
+                console.error("Failed to fetch teachers:", error);
+            }
+        };
+        fetchTeachers();
+    }, [searchParams.toString()]); // Trigger effect on searchParams change
+
     return (
         <div className=" flex flex-col gap-6">
             <header className="flex flex-col gap-4 lg:flex-row justify-between items-start ">
@@ -23,7 +54,13 @@ export default function Home() {
                 </div>
             </header>
             <hr className="border-t border-hr" />
-            <Table />
+            {loading ? (
+                <div className="flex items-center justify-center pt-10 w-full bg-bgmain">
+                    <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-secondary"></div>
+                </div>
+            ) : (
+                <Table teachers={teachers} />
+            )}
         </div>
     );
 }
